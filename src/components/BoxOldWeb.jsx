13e33feff501c637f32a485220b6f17e37abaaa6
RizkiOfc <tururizki21@gmail.com>
2025-05-30 05:14:57 +0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -3,7 +3,7 @@ const BoxOldWeb = () => {
 		<div id="BoxOldWeb">
 			<a href="https://www.tiktok.com/@spenzel.8a?_t=ZS-8tox9idUplP&_r=1">
 				<div className="flex justify-between">
-					<img src="/link.png" alt="" className="w-10 h-10" />
+					<i class="ri-tiktok-fill"></i>
 					<img src="/next.png" alt="" className="h-3 w-3" />
 				</div>
 				<h1 className="text-white text-base font-semibold pr-0 mt-5">Class Tiktok</h1>
